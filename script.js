8388ea118fcae2cab49deef155e2224dadd623da
suryadiekacipta <suryadiekac@gmail.com>
2026-07-22 03:29:14 +0700

    Add initial HTML structure, CSS styles, and JavaScript functionality for calculator app

---
+++ script.js
@@ -0,0 +1,40 @@
+let input1 = document.getElementById("input1");
+let isi1;
+let sisa_token = document.querySelector(".token");
+let token = 5;
+let pop_token1 = document.querySelector(".div_token");
+let buton1 = document.getElementById("button1");
+function hitung(data) {
+  isi1 = input1.value += data;
+}
+
+function clearall() {
+  input1.value = "";
+}
+
+function hapus() {
+  input1.value = input1.value.slice(0, -1);
+}
+
+function hasil() {
+  try {
+    let hasil1 = eval(input1.value);
+
+    input1.value = hasil1;
+  } catch (error) {
+    console.log("error");
+  }
+  token--;
+  sisa_token.textContent = "Token: " + token;
+
+  if (token == 0 || token < 0) {
+    pop_token1.classList.add("active");
+    sisa_token.textContent = "Token Habis!";
+    input1.value = "Isi Token Dulu Yaa☺️";
+    return;
+  }
+}
+
+buton1.addEventListener("click", () => {
+  pop_token1.classList.remove("active");
+});
