b5550a17df770b788858b1e38e86fce6d98e0b04
suryadiekacipta <suryadiekac@gmail.com>
2026-07-22 20:09:19 +0700

    Add watch ads prompt and related functionality to the calculator app

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let input1 = document.getElementById("input1");
-let isi1;
 let sisa_token = document.querySelector(".token");
 let token = 5;
 let pop_token1 = document.querySelector(".div_token");
 let buton1 = document.getElementById("button1");
+
+function bersihkan() {
+  if (
+    input1.value == "Isi Token Dulu Yaa☺️" ||
+    input1.value == "Error!" ||
+    input1.value == "undefined"
+  ) {
+    input1.value = "";
+  }
+}
+
 function hitung(data) {
-  isi1 = input1.value += data;
+  bersihkan();
+  input1.value += data;
 }
 
 function clearall() {
@@ -23,6 +34,7 @@ function hasil() {
     input1.value = hasil1;
   } catch (error) {
     console.log("error");
+    input1.value = "Error!";
   }
   token--;
   sisa_token.textContent = "Token: " + token;
@@ -43,14 +55,31 @@ buton1.addEventListener("click", () => {
 let refill = document.querySelector(".div_add");
 let iklan1 = document.querySelector(".div_iklan");
 let video1 = document.getElementById("video1");
+let pop_choice = document.querySelector(".div_nonton");
+
+let buttonYes = document.getElementById("yes");
+let buttonNo = document.getElementById("no");
 
 refill.addEventListener("click", () => {
+  pop_choice.classList.add("active3");
+});
+
+buttonYes.addEventListener("click", () => {
   iklan1.classList.add("active2");
   video1.play();
-  token += 5;
+  if (token > 0) {
+    token += 5;
+  } else if (token <= 0) {
+    token = 5;
+  }
   sisa_token.textContent = "Token: " + token;
   setTimeout(() => {
     iklan1.classList.remove("active2");
     video1.pause();
-  }, 12400);
+  }, 12250);
+  pop_choice.classList.remove("active3");
+});
+
+buttonNo.addEventListener("click", () => {
+  pop_choice.classList.remove("active3");
 });
