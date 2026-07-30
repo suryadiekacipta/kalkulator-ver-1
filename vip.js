2f486884133b970597a5fcf1d8b40970138dfce9
suryadiekacipta <suryadiekac@gmail.com>
2026-07-31 02:15:17 +0700

    feat: update input handling and add lyrics display functionality

--- vip.js
+++ vip.js
@@ -26,32 +26,105 @@ addEventListener("load", function () {
   console.log(waktu_aktif);
 });
 
-let input = document.getElementById("input1");
+let input1 = document.getElementById("input1");
 
 function hitung(value) {
   cek();
-  input.value += value;
+  input1.value += value;
 }
 
 function hapus_semua() {
-  input.value = "";
+  input1.value = "";
 }
 
 function hapus() {
-  input.value = input.value.slice(0, -1);
+  input1.value = input.value.slice(0, -1);
 }
 
 function hasil() {
   try {
-    eval(input.value);
+    let hasil1 = eval(input1.value);
+    input1.value = hasil1;
   } catch (error) {
     console.log("error");
-    input.value = "Error!";
+    input1.value = "Error!";
   }
 }
 
 function cek() {
-  if (input.value == "undefined" || input.value == "Error!") {
-    input.value = "";
+  if (input1.value == "undefined" || input1.value == "Error!") {
+    input1.value = "";
   }
+}
+
+let lagu1 = document.getElementById("lagu1");
+
+lagu1.addEventListener("click", () => {
+  lirik_dunia_yg_nanti();
+});
+function lirik_dunia_yg_nanti() {
+  let clear_time;
+  clearTimeout(clear_time);
+  input1.value = "🎶 Dunia Yang Nanti";
+
+  clear_time = setTimeout(() => {
+    input1.value = "Waktu bawa kita ke tempat tak";
+  }, 12500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "terduga, Kau hadir kembali";
+  }, 18000);
+
+  clear_time = setTimeout(() => {
+    input1.value = "menyapa hidupku, Kau ada yang";
+  }, 23500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "miliki, Ku ada yang punyai";
+  }, 30500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "Juga ada rasa yang belum";
+  }, 36000);
+
+  clear_time = setTimeout(() => {
+    input1.value = "sempat terucap, Tak banyak";
+  }, 40500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "inginku, tak banyak inginmu";
+  }, 47000);
+
+  clear_time = setTimeout(() => {
+    input1.value = "Kita dua hati yang bodoh";
+  }, 51900);
+
+  clear_time = setTimeout(() => {
+    input1.value = "berjalan pada waktu";
+  }, 57000);
+
+  clear_time = setTimeout(() => {
+    console.log("jalanbos");
+    input1.value = "Itulah kenapa jatuh cinta";
+  }, 64000);
+
+  clear_time = setTimeout(() => {
+    input1.value = "di katakan jatuh, karena";
+  }, 68900);
+
+  clear_time = setTimeout(() => {
+    input1.value = "sebagaimana kita jatuh";
+  }, 72500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "Kita tidak punya kuasa";
+  }, 76500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "Pilih jatuh pada hati yang mana";
+  }, 80500);
+
+  clear_time = setTimeout(() => {
+    input1.value = "🎶";
+  }, 90000);
 }
