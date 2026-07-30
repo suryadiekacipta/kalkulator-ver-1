1092f8de28880c1dc976debd448103c8971d2570
suryadiekacipta <suryadiekac@gmail.com>
2026-07-30 18:00:15 +0700

    feat: add calculator interface with input and button functionality

--- vip.js
+++ vip.js
@@ -25,3 +25,33 @@ addEventListener("load", function () {
   waktu_aktif.textContent = tanggal + "-" + bulan_name[bulan] + "-" + tahun;
   console.log(waktu_aktif);
 });
+
+let input = document.getElementById("input1");
+
+function hitung(value) {
+  cek();
+  input.value += value;
+}
+
+function hapus_semua() {
+  input.value = "";
+}
+
+function hapus() {
+  input.value = input.value.slice(0, -1);
+}
+
+function hasil() {
+  try {
+    eval(input.value);
+  } catch (error) {
+    console.log("error");
+    input.value = "Error!";
+  }
+}
+
+function cek() {
+  if (input.value == "undefined" || input.value == "Error!") {
+    input.value = "";
+  }
+}
